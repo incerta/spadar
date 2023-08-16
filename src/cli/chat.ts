43ce07a5127124eb6e3aadce09bd76b6a1c74afa
Konstantin Mazur <incerta.dei@gmail.com>
2023-08-16 03:52:30 +0200

    Create an idea for chat initial message API

--- src/cli/chat.ts
+++ src/cli/chat.ts
@@ -16,6 +16,12 @@ export default command(
     },
   },
   (argv) => {
+    // IDEA: I guess ability to send initial message should be alongside
+    // the other kind of response, it should be compatible for usage
+    // as cli tool in the pipe, e.g. takes text input and sends right output
+    //
+    // For now we consider this API feature as mean for creation
+    // imperatively driven chat assistant quickly
     const initialMessage = argv._.length > 0 ? argv._.join(' ').trim() || undefined : undefined
 
     return initialMessage ? startConversation([{ role: 'user', content: initialMessage }]) : startConversation()
