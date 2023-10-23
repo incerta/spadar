87f11c6fb752a781d0d92ee7919f8d1f29128b96
Konstantin Mazur <incerta.dei@gmail.com>
2023-10-23 16:52:07 +0200

    Change app running terminal tab to "Spadar" title

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,6 +3,9 @@ import { version } from '../../package.json'
 
 import chat from './chat'
 
+// Set terminal tab title
+process.stdout.write('\x1b]0;Spadar\x07')
+
 cli({
   name: 'spadar',
   version: version,
