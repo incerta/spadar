44b378924357ae96c490ee3d20ecdafa8b2d2c32
Konstantin Mazur <incerta.dei@gmail.com>
2023-12-17 00:02:37 +0100

    Handle `spadar --help` and `spadar --version` flags

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,5 @@
 // TODO move file to `src/cli-entry-point.ts`
+import dedent from 'dedent'
 import config from '../config'
 import { SpadarError } from '../utils/error'
 import { collectFlags } from '../utils/command-line'
@@ -31,13 +32,68 @@ function cliRouter(argv: string[]) {
   }
 
   if (params[0][0] === '-') {
-    if (params[0] !== '-h' && params[0] !== '--help') {
-      throw new SpadarError(
-        `Only --help or -h command are allowed at the level`
-      )
+    const flag = params[0]
+
+    if (flag === '-v' || flag === '--version') {
+      console.log(config.version)
+      return
     }
-    // FIXME: make help for the `spadar` cli root layer
-    console.log('Help is under construction')
+
+    if (flag === '-h' || flag === '--help') {
+      // TODO: show the number of available adapters for the given
+      //       transformation type in the parenthesis like:
+      //       "adapters for text to text transformation (12)"
+
+      const helpMessage = dedent(`
+        Usage: spadar [flag]
+
+          -h, --help                  output usage information
+          -v, --version               output the version number
+
+        Usage: spadar [command]
+
+          adapter                     generate/validate/use the external adapter modules
+          chat                        start chat with with specific textToText adapter
+
+        Usage: spadar [transformation]
+
+          CLI stdin and stdout for data transformation by external adapter module
+
+          Examples:  echo "Hello!" | spadar textToText spadar-adapter GPT staticToStatic > file.txt
+                     cat image.jpg | spadar imageToImage spadar-adapter DALL-E staticToStatic > file.jpg
+   
+        Transformations:
+
+          textToText                  adapters for text to text transformation
+          textToImage                 adapters for text to image transformation
+          textToAudio                 adapters for text to audio transformation
+          textToVideo                 adapters for text to video transformation
+
+          imageToText                 adapters for image to text transformation
+          imageToImage                adapters for image to image transformation
+          imageToAudio                adapters for image to audio transformation
+          imageToVideo                adapters for image to video transformation
+
+          audioToText                 adapters for audio to text transformation
+          audioToImage                adapters for audio to image transformation
+          audioToAudio                adapters for audio to audio transformation
+          audioToVideo                adapters for audio to video transformation
+
+          videoToText                 adapters for video to text transformation
+          videoToImage                adapters for video to image transformation
+          videoToAudio                adapters for video to audio transformation
+          videoToVideo                adapters for video to video transformation
+
+          Get detailed help on each transformation by: spadar [transformation] --help
+    `)
+
+      console.log('\n' + helpMessage + '\n')
+      return
+    }
+
+    console.log(`Unsupported flag: ${flag}`)
+
+    return
   }
 
   if (params[0] === 'chat') {
