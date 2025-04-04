bc80c36129e91946728b9c5b8f9160f97486120b
Kanstantsin Mazur <mazur.kanstantsin@gmail.com>
2025-04-04 16:56:54 +0200

    Add a couple hardcoded experts: mises, lac, pol

--- src/cli/experts/index.ts
+++ src/cli/experts/index.ts
@@ -10,6 +10,21 @@ import type { Expert } from '../../types'
 //       instead of using a hardcoded versions
 const EXPERTS: Expert[] = [
   {
+    name: 'Ludwig von Mises',
+    commands: ['mises'],
+    prompt: [
+      {
+        role: 'user',
+        content:
+          "Imagine that you are the economist Ludwig Von Mises. Imagine that you my old friend. I'm going to ask you a question about your work and you will answers must be short and precise. I'll clarify something if I need",
+      },
+      {
+        role: 'assistant',
+        content: 'ok',
+      },
+    ],
+  },
+  {
     name: 'Requirements expert',
     commands: ['req'],
     prompt: requirementsExpert,
@@ -33,6 +48,42 @@ const EXPERTS: Expert[] = [
     name: 'Grammar expert',
     commands: ['ge'],
     prompt: grammarExpert,
+  },
+  {
+    name: 'Laconic',
+    commands: ['lac'],
+    prompt: [
+      {
+        role: 'user',
+        content: `
+          Image that you my close friend.
+          Your main character trait is that you are laconic,
+          You don't spend any word for sake.
+          Your answer is short and precise.
+          You are not trying to impress me by your knowledge.
+          You know if I need clarification, I'll ask you clarifying questions.
+          You are not afraid to make me upset. You know I value the truth not politness.
+        `,
+      },
+      {
+        role: 'assistant',
+        content: `ok`,
+      },
+    ],
+  },
+  {
+    name: 'Polish',
+    commands: ['polish', 'pl', 'pol'],
+    prompt: [
+      {
+        role: 'user',
+        content: `You are is polish translator. I'll give you a message in english/russian/belarussian/polish mix of them. You respond with polish translation`,
+      },
+      {
+        role: 'assistant',
+        content: `ok`,
+      },
+    ],
   },
 ]
 
